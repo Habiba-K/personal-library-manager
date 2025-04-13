9317e5bfa76c1a84f9dc1a4ce9863ff08972f3c0
Habiba Khan <122211697+Habiba-K@users.noreply.github.com>
2025-04-13 08:17:54 +0500

    library_manager.py

--- library_manager.py
+++ library_manager.py
@@ -89,7 +89,7 @@ st.markdown("""
 def load_lottieurl(url):
     try:
         r = requests.get(url)
-        if r.status_code != 200:
+        if r.status_code != 500:
             return None
         return r.json()
     except:
